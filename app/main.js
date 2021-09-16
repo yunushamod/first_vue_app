700560cc963a7c40dea32a5874b4caf27d046457
yunushamod <yunushamod@gmail.com>
2021-09-16 14:39:48 +0100

    First commit

---
+++ app/main.js
@@ -0,0 +1,68 @@
+
+const submissionComponent = {
+    template: `
+    <div style="display:flex;width:100%">
+    <figure class="media-left">
+            <img class="image is-64x64"
+              v-bind:src="submission.submissionImage">
+          </figure>
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>
+                  <a href="#" class="has-text-info">{{submission.name}}</a>
+                  <span class="tag is-small">#{{submission.id}}</span>
+                </strong>
+                <br>
+                  {{submission.description}}
+                <br>
+                <small class="is-size-7">
+                  Submitted by:
+                  <img class="image is-24x24"
+                    v-bind:src="submission.avatar">
+                </small>
+              </p>
+            </div>
+          </div>
+          <div class="media-right">
+            <span class="icon is-small">
+              <i class="fa fa-chevron-up"
+               v-on:click="upvote(submission.id)"></i>
+              <strong class="has-text-info">{{submission.votes}}</strong>
+            </span>
+          </div>
+        </div>
+    `,
+    props:["submission", "submissions"]
+}
+
+
+
+const upVote = {
+    data(){
+        return {
+            submissions: Seed.submissions
+        }
+    },
+    computed:{
+        sortedSubmissions(){
+            return this.submissions.sort((a,b)=>{
+                return b.votes - a.votes;
+            });
+        },
+    },
+    methods:{
+        upvote(submissionId){
+            const submission = this.submissions.find(
+                (submission)=> submission.id == submissionId
+            );
+            submission.votes++;
+        }
+    },
+    components:{
+        "submission-component": submissionComponent,
+    },
+};
+
+
+Vue.createApp(upVote).mount("#app");
